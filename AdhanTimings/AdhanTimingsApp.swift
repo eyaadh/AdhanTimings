4f0746de03a010ed47ccbe6329e43423adf8dba9
eyaadh <eyaadh@gmail.com>
2022-04-02 11:54:13 +0500

    Initial Commit

---
+++ AdhanTimings/AdhanTimingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhanTimingsApp.swift
+//  AdhanTimings
+//
+//  Created by Ahmed Iyad on 2-4-22.
+//
+
+import SwiftUI
+
+@main
+struct AdhanTimingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
